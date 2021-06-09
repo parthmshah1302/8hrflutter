67e7388b9cf28d451298aefb8abec40cd8f96787
Parth Shah <parthmshah1302@gmail.com>
2021-06-09 22:17:07 +0530

    first: First commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Parth());
+}
+
+class Parth extends StatelessWidget {
+  const Parth({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text('Hello, world!'),
+          ),
+        ),
+      ),
+    );
+  }
+}
