b38a34b4b12c4e3d7797594738953e18d26827b9
Parth Shah <parthmshah1302@gmail.com>
2021-07-08 10:57:35 +0530

    learnt: +catalog.dart and models

---
+++ lib/models/catalog.dart
@@ -0,0 +1,28 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
+
+final products = [
+  Item(
+    id: "001",
+    name: "iPhone 12 Pro",
+    desc: "Apple iPhone 12th Generation",
+    color: "#33505a",
+    price: 999,
+    image:
+        "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-pro-family-hero?wid=940&hei=1112&fmt=jpeg&qlt=80&.v=1604021663000",
+  )
+];
